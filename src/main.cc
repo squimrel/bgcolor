35a762f182772450b6f1fb497e4b8f4e0e919c69
squimrel <squimrel@users.noreply.github.com>
2017-01-02 08:53:33 +0000

    Output rgba format if alpha is set

--- src/main.cc
+++ src/main.cc
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cmath>  // std::round
+
 #include <iomanip>   // std::setw, std::fill, std::hex
 #include <iostream>  // std::cerr
-#include <string>    // std::string
+#include <string>    // std::string, std::to_string
 #include <tuple>     // std::tie
 
 #include "./src/png.h"
@@ -22,12 +24,15 @@
 
 int main(int argc, const char* argv[]) {
   using std::cerr;
+  using std::cout;
   using std::setw;
+  using std::string;
+  using std::to_string;
   if (argc < 2) {
     cerr << "Usage: " << argv[0] << " [image]\n";
     return 1;
   }
-  const std::string filename = argv[1];
+  const string filename = argv[1];
   Png png;
   try {
     png.open(filename);
@@ -36,7 +41,12 @@ int main(int argc, const char* argv[]) {
     int blue;
     double alpha;
     std::tie(red, green, blue, alpha) = png.color(0, 0);
-    alpha /= static_cast<double>(0xff);
+    // Round to the nearset 0.05.
+    alpha = std::round((alpha / static_cast<double>(0xff)) * 20) / 20;
+    // This might not be used.
+    const string rgba = "rgba(" + to_string(red) + "," + to_string(green) +
+                        "," + to_string(blue) + "," + from_stream(alpha) +
+                        ")\n";
     // Assume a white background color to generate a hex color.
     if (alpha != 1) {
       const int white = 0xff * (1 - alpha);
@@ -44,8 +54,11 @@ int main(int argc, const char* argv[]) {
       green = green * alpha + white;
       blue = blue * alpha + white;
     }
-    std::cout << "#" << std::setfill('0') << std::hex << setw(2) << red
-              << setw(2) << blue << setw(2) << green << '\n';
+    cout << "#" << std::setfill('0') << std::hex << setw(2) << red << setw(2)
+         << blue << setw(2) << green << '\n';
+    if (png_get_channels(png.data->structure, png.data->info) == 4) {
+      cout << rgba;
+    }
   } catch (const std::runtime_error& err) {
     cerr << err.what();
     // I'm not sure if interupting the control flow here is actually allowed.
